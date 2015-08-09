98fe4afc3a0f46dfb72f89247aaeecb07de7a4dc
downup2u <xiaoqingwang@126.com>
2015-08-10 00:16:23 +0800

    新增

--- yunqiapp/lib/control.js
+++ yunqiapp/lib/control.js
@@ -16,3 +16,33 @@ Router.route('/more', function () {
     this.layout('mainlayout');
     this.render('more', {to: 'content'});
 });
+
+Router.route('/homedetail/wyxd', function () {
+    this.layout('indexdetailpagelayout',{data: {title: '我要下单'}});
+    this.render('wyxd', {to: 'detailpagecontent'});
+});
+
+Router.route('/homedetail/wddd', function () {
+    this.layout('indexdetailpagelayout',{data: {title: '我的订单'}});
+    this.render('wddd', {to: 'detailpagecontent'});
+});
+
+Router.route('/homedetail/cxhd', function () {
+    this.layout('indexdetailpagelayout',{data: {title: '促销活动'}});
+    this.render('cxhd', {to: 'detailpagecontent'});
+});
+
+Router.route('/homedetail/yqcx', function () {
+    this.layout('indexdetailpagelayout',{data: {title: '用气查询'}});
+    this.render('yqcx', {to: 'detailpagecontent'});
+});
+
+Router.route('/homedetail/wdhb', function () {
+    this.layout('indexdetailpagelayout',{data: {title: '我的红包'}});
+    this.render('wdhb', {to: 'detailpagecontent'});
+});
+
+Router.route('/homedetail/yhq', function () {
+    this.layout('indexdetailpagelayout',{data: {title: '优惠券'}});
+    this.render('yhq', {to: 'detailpagecontent'});
+});
